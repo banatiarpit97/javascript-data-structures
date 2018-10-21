9982fdeb11d8380a44df887bbe4288b4c0df75df
arpit <banatiarpit97@yahoo.co.in>
2018-10-22 04:05:58 +0530

    .

--- 0_between_1.js
+++ 0_between_1.js
@@ -4,8 +4,8 @@ function between(){
     for(let i=0;i<str.length;i++){
         if(str[i] == 0){
             if(str[i-1] == 1 && str[i+1] == 1){
-              console.log('true',i);
-              return;
+                console.log('true',i);
+                return;
             }
         }
     }
