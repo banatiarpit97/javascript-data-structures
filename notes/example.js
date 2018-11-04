bff012f988c7e1215d48046dc526a9025a96f702
arpit <banatiarpit97@yahoo.co.in>
2018-11-04 16:59:18 +0530

    oops chapter

--- notes/example.js
+++ notes/example.js
@@ -1,8 +1,16 @@
-var a = "gg";
-
-{   
-    let a = "gs";
-
-    console.log(a)
+var circle = {
+    init:function(radius){
+        this.radius = radius;
+    },
+    diamater:function(){
+        return this.radius*2;
+    }
 }
-    console.log(a)
+var semicircle = Object.create(circle);
+semicircle.area = function(){
+    return ((3.14*this.radius*this.radius)/2);
+}
+
+var s1 = Object.create(semicircle);
+s1.init(3);
+console.log(s1.area());
