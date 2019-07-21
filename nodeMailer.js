27df4b8733370ccaa5f2c5a5b15978304b675c6a
Arpit Banati <banatiarpit97@yahoo.co.in>
2019-07-22 01:21:29 +0530

    Update nodeMailer.js

--- nodeMailer.js
+++ nodeMailer.js
@@ -4,7 +4,7 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'arpitbanati97@gmail.com',
-        pass: 'laptoplaptop'
+        pass: 'password'
     }
 });
 
